236fe63a55e0ba5dc042f6960b5e115645f288a5
rohiththalari-commits <rohiththalari@gmail.com>
2026-01-04 20:58:01 +0530

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -20,9 +20,6 @@ export async function getServerSideProps({ res }) {
 export default function Sitemap() {
   return null;
 }
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
   <url>
     <loc>https://pincode-search-kohl.vercel.app/pincode/500001</loc>
   </url>
