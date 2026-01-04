4b39b5899ca0b20db8d89e1c05e90df9855ec37d
rohiththalari-commits <rohiththalari@gmail.com>
2026-01-04 11:58:01 +0530

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="PASTE_YOUR_GOOGLE_CODE_HERE"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
